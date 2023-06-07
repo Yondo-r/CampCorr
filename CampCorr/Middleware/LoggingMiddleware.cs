745bc22c50f6e77c76c98f64fbce7d40ec096947
Yondo-r <yondo.ricardo@gmail.com>
2023-06-07 09:37:11 -0300

    Adicionando funcao de log

---
+++ CampCorr/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.Logging;
+namespace CampCorr.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LoggingMiddleware> _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                // Executa a próxima etapa do pipeline
+                await _next(context);
+
+                // Registra o log de sucesso no banco de dados
+                LogAccess(context, false);
+            }
+            catch (Exception ex)
+            {
+                // Registra o log de erro no banco de dados
+                LogAccess(context, true);
+
+                // Re-levanta a exceção para que o tratamento de erro padrão seja executado
+                throw;
+            }
+        }
+
+        private void LogAccess(HttpContext context, bool isError)
+        {
+            string controller = context.Request.RouteValues["controller"]?.ToString();
+            string action = context.Request.RouteValues["action"]?.ToString();
+            string login = context.User.Identity.Name;
+            string ip = context.Connection.RemoteIpAddress.ToString();
+
+            if (isError)
+            {
+                _logger.LogError("Erro na action {Controller}/{Action}. Login: {Login}, IP: {IP}", controller, action, login, ip);
+            }
+            else
+            {
+                _logger.LogInformation("Acesso bem-sucedido na action {Controller}/{Action}. Login: {Login}, IP: {IP}", controller, action, login, ip);
+            }
+
+            // Grava os logs no banco de dados conforme necessário
+            // Exemplo:
+            // dbContext.Logs.Add(new LogEntry { Controller = controller, Action = action, Login = login, IP = ip, IsError = isError });
+            // dbContext.SaveChanges();
+        }
+    }
+
+}
--- CampCorr/Program.cs
+++ CampCorr/Program.cs
@@ -1,4 +1,5 @@
 using CampCorr.Context;
+using CampCorr.Middleware;
 using CampCorr.Repositories;
 using CampCorr.Repositories.Interfaces;
 using CampCorr.Services;
@@ -78,6 +79,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseMiddleware<LoggingMiddleware>();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
